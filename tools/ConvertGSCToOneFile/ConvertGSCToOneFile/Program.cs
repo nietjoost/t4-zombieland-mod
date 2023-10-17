ba4ab8a0ae1e15ae071566565cd19cc2083f0e88
Joost de Niet <nietjoost@gmail.com>
2023-10-17 20:25:35 +0200

    Added some colors and invalid folder logic

--- tools/ConvertGSCToOneFile/ConvertGSCToOneFile/Program.cs
+++ tools/ConvertGSCToOneFile/ConvertGSCToOneFile/Program.cs
@@ -1,5 +1,10 @@
 ﻿using static System.Net.Mime.MediaTypeNames;
 
+const string CYAN = "\x1b[96m";
+const string RED = "\x1b[91m";
+const string GREEN = "\x1b[92m";
+const string NORMAL = "\x1b[39m";
+
 // Console start message
 Console.WriteLine("Welcome to the RooieRonnie's GSC project to one file converter tool");
 Console.WriteLine("Paste the folder path to the init.gsc file:");
@@ -10,13 +15,19 @@ var folderPlace = Console.ReadLine();
 // If folder is null, stop the process
 if (folderPlace == null || folderPlace.Length == 0)
 {
-    Console.WriteLine("No folder given!");
+    Console.WriteLine($"{RED}No folder given!{NORMAL}");
     return;
 }
 
 if (Directory.Exists(folderPlace) == false)
 {
-    Console.WriteLine("The given folder does not exists!");
+    Console.WriteLine($"{RED}The given folder does not exists!{NORMAL}");
+    return;
+}
+
+if (File.Exists($"{folderPlace}/init.gsc") == false)
+{
+    Console.WriteLine($"{RED}The given folder does not contain an valid start file (init.gsc)!{NORMAL}");
     return;
 }
 
@@ -31,6 +42,7 @@ var cleanedFiles = files.Where(f => !f.Contains(".git"))
     .Where(f => !f.Contains("callbacks.gsc"))
     .Where(f => f.Contains(".gsc"))
     .ToList();
+Console.WriteLine($"Total files found: {GREEN}{cleanedFiles.Count}{NORMAL}");
 
 // Set variables for the custom map edits
 var loadMapCount = 1;
@@ -55,6 +67,7 @@ if (File.Exists(path))
 // Check each line if we want to include it in the file
 foreach (var file in cleanedFiles)
 {
+    Console.WriteLine($"Converting file {CYAN}{Path.GetFileName(file)}{NORMAL}");
     string text = File.ReadAllText(file);
     string[] lines = text.Split(Environment.NewLine);
 
@@ -97,3 +110,6 @@ foreach (var file in cleanedFiles)
 string content = File.ReadAllText(path);
 content = "#include maps\\mp\\_utility;" + "\n" + "#include maps\\mp\\gametypes\\_hud_util;" + "\n" + content;
 File.WriteAllText(path, content);
+
+Console.WriteLine();
+Console.WriteLine($"File is saved to: {GREEN}{path}{NORMAL}");
